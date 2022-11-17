c16f9e71a42003f77502f8913e0de3b492326954
Wakwakin <76051659+Kino24@users.noreply.github.com>
2022-11-17 19:16:24 +0800

    Update programmingProblem2.py

--- LR1/programmingProblem2.py
+++ LR1/programmingProblem2.py
@@ -3,11 +3,15 @@ fileStream=open(file,'r').readlines()
 choice=0
 print("The file has",len(fileStream)," lines")
 while(1):
+    print("The file has",len(fileStream)," lines")
     choice=int(input("Select line to read, Enter 0 to exit the program: "))
-    if choice==0:
-        break
-    choice=choice-1
-    print(fileStream[choice])
+    if choice <= len(fileStream) and choice > -1:
+        if choice == 0:
+            break
+        choice=choice-1
+        print(fileStream[choice])
+    else:
+        print("The line does not exist! Please enter another line number")
 
 print("Thank you for using the program!")
 input("Press Enter to Continue...")
