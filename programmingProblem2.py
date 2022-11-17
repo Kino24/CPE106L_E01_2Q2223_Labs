f3659c01b856c0b5653289de07bb53ecdff840aa
Wakwakin <76051659+Kino24@users.noreply.github.com>
2022-11-17 18:01:13 +0800

    Updated programmingProblem2.py
    made changes to line 4 since it was not printing the proper amount of lines in the present file.

--- programmingProblem2.py
+++ programmingProblem2.py
@@ -1,8 +1,7 @@
 file=input("Enter File Name: ")
-fileName=file+'.txt'
 fileStream=open(fileName,'r').readlines()
 choice=0
-print("The file has",len(file)," lines")
+print("The file has",len(fileName)," lines")
 while(1):
     choice=int(input("Select line to read, Enter 0 to exit the program: "))
     if choice==0:
